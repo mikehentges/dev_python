0b98f8c996aee6ea83d75dda54d25ca0f727b69d
Michael Hentges <5008447+mikehentges@users.noreply.github.com>
2023-12-25 18:33:12 -0600

    added pytest, ready for baselining into a dev container

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("Hello World")
+
+
+def my_fun(x):
+    print(f"in a function {x}")
+
+
+my_fun("12")
---
+++ test/main_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+@pytest.fixture
+def example_fixture():
+    return 1
+
+
+def test_always_passes():
+    assert True
+
+
+def test_always_fails():
+    assert False
+
+
+def test_with_fixture(example_fixture):
+    assert example_fixture == 1
